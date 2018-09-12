1138af8f8512c8c0f0477089fde421eea613ae63
erickpeirson <brp53@cornell.edu>
2018-09-12 10:36:17 -0400

    incremented rc

--- users/setup.py
+++ users/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='arxiv-auth',
-    version='0.1.0rc13',
+    version='0.1.0rc14',
     packages=[f'arxiv.{package}' for package
               in find_packages('./arxiv', exclude=['*test*'])],
     install_requires=[
