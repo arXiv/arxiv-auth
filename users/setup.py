28ca8bd7adaba94318f97bc2256e874ad9627dd4
erickpeirson <brp53@cornell.edu>
2018-12-20 10:11:13 -0500

    updated to base v0.12.1

--- users/setup.py
+++ users/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='arxiv-auth',
-    version='0.2.0rc2',
+    version='0.2.1',
     packages=[f'arxiv.{package}' for package
               in find_packages('./arxiv', exclude=['*test*'])],
     install_requires=[
