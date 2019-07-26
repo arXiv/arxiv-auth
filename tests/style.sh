263f694bdeb8d5d60ef3ca55c079b970bf882ca4
erickpeirson <brp53@cornell.edu>
2019-07-26 15:45:47 -0400

    minor

--- tests/style.sh
+++ tests/style.sh
@@ -7,6 +7,7 @@ PROJECT=$1
 pipenv run pydocstyle --convention=numpy --add-ignore=D401 ${PROJECT}
 PYDOCSTYLE_STATUS=$?
 if [ $PYDOCSTYLE_STATUS -ne 0 ]; then PYDOCSTYLE_STATE="failure" && echo "pydocstyle failed"; else PYDOCSTYLE_STATE="success" &&  echo "pydocstyle passed"; fi
+if [ "$TRAVIS_PULL_REQUEST_SHA" = "" ];  then SHA=$TRAVIS_COMMIT; else SHA=$TRAVIS_PULL_REQUEST_SHA; fi
 
 if [ -z ${GITHUB_TOKEN} ]; then
     echo "Github token not set; will not report results";
--- tests/type-check.sh
+++ tests/type-check.sh
@@ -7,7 +7,7 @@ PROJECT=$1
 touch ${PROJECT}/__init__.py
 MYPY_STATUS=$( pipenv run mypy -p ${PROJECT} | grep -v "test.*" | tee /dev/tty | wc -l | tr -d '[:space:]' )
 if [ $MYPY_STATUS -ne 0 ]; then MYPY_STATE="failure" && echo "mypy failed"; else MYPY_STATE="success" &&  echo "mypy passed"; fi
-
+if [ "$TRAVIS_PULL_REQUEST_SHA" = "" ];  then SHA=$TRAVIS_COMMIT; else SHA=$TRAVIS_PULL_REQUEST_SHA; fi
 if [ -z ${GITHUB_TOKEN} ]; then
     echo "Github token not set; will not report results";
 else
