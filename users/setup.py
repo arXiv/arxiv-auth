2de7be82d337f459511594dd1d43eb59ebd04e27
erickpeirson <brp53@cornell.edu>
2019-07-26 15:47:10 -0400

    set version

--- users/setup.py
+++ users/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='arxiv-auth',
-    version='0.3.5rc2',
+    version='0.4.1',
     packages=[f'arxiv.{package}' for package
               in find_packages('./arxiv', exclude=['*test*'])],
     scripts=['bin/generate-token'],
