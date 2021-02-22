f0710dc152e6de89ed539a5328023ac8e1ed7182
Brian D. Caruso <bdc34@cornell.edu>
2021-02-22 15:32:55 -0500

    Fixes to GCP deploy

--- accounts/deploy/redis.sh
+++ accounts/deploy/redis.sh
@@ -7,7 +7,7 @@ source config.sh
 
 #TODO For phoenix-arxiv we could probably do just a super small vm+container
 gcloud services enable redis.googleapis.com
-gcloud redis create auth-jwt-store \
+gcloud redis instances create auth-jwt-store \
        --tier=basic \
        --redis-version=redis_4_0 \
        --region=$REGION \
